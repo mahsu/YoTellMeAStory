e38f188633bbe4ec650cc3d99ea6476e651db183
Matthew Hsu <gears88@gmail.com>
2014-11-15 19:36:20 -0500

    fixed path references moved function distance to yo.js

--- routes/yo.js
+++ routes/yo.js
@@ -39,4 +39,23 @@ router.get('/location', function(req, res) {
 
 });
 
+function distance(loc1, loc2) {
+    var lat1 = loc1[0];
+    var lon1 = loc1[1];
+    var lat2 = loc2[0];
+    var lon2 = loc2[1];
+    var R = 6371; // km
+    var angle1 = lat1 * Math.PI / 180;
+    var angle2 = lat2 * Math.PI / 180;
+
+    var deltaAngle = (lat2-lat1) * Math.PI / 180;
+    var deltaLong = (lon2-lon1)  * Math.PI / 180;
+
+    var a = Math.sin(deltaAngle/2) * Math.sin(deltaAngle/2) +
+        Math.cos(angle1) * Math.cos(angle2) *
+        Math.sin(deltaLong/2) * Math.sin(deltaLong/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    return R * c;
+}
+
 module.exports = router;
